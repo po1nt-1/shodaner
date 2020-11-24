e5322c4aa2469ceb144f1584477a0a8bd015a676
po1nt-1 <po1nt@protonmail.ch>
2020-11-24 21:33:32 +1000

    read&write csv

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import inspect
+import os
+import sys
 import time
+from pprint import pprint
 
 import shodan
 
 import creepy
+
+
+class error(Exception):
+    pass
+
 
 SHODAN_API_KEY = creepy.read_token()  # input()
 # creepy.write_token(SHODAN_API_KEY)
@@ -18,27 +27,46 @@ api = shodan.Shodan(SHODAN_API_KEY)
 #             data.append(row)
 #     return data
 
+def _get_script_dir(follow_symlinks=True) -> str:
+    '''получить директорию со скриптом'''
 
-# def csv_writer(data, path):
-#     try:
-#         with open(path, 'a', newline='',) as csv_file:
-#             writer = csv.DictWriter(csv_file)
-#             if os.path.getsize(new_file_dir) == 0:
-#                 writer.writeheader()
-#             for row in data:
-#                 writer.writerow(row)
-#         del data
-#         return None
-#     except Lil_error as e:
-#         raise Lil_error(str(e))
+    # https://clck.ru/P8NUA
+    if getattr(sys, 'frozen', False):  # type: ignore
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(_get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+
+
+def csv_writer(data, name='default.csv'):
+    path = os.path.join(_get_script_dir(), name)
+    headers = '|'.join(data.keys())
+    vals = '|'.join(data.values())
+
+    with open(path, 'w', encoding="utf-8") as f:
+        f.write(headers + '\n' + vals + '\n')
+
+
+def csv_reader(name='default.csv'):
+    path = os.path.join(_get_script_dir(), name)
+    with open(path, 'r', encoding='utf-8') as f:
+        data = [line[:-1].split('|') for line in f.readlines()]
+
+    return dict(zip(data[0], data[1]))
+
+
+def fix_slash(string):
+    return str(string).replace('\n', '\\n').replace('\r', '\\r')
 
 
 def shodan_info():
     r = api.info()
     result = {
-        'plan': r.get('plan'),
-        'query_credits': r.get('query_credits'),
-        'scan_credits': r.get('scan_credits')
+        'plan': fix_slash(r.get('plan')),
+        'query_credits': fix_slash(r.get('query_credits')),
+        'scan_credits': fix_slash(r.get('scan_credits'))
     }
 
     return result
@@ -49,22 +77,48 @@ def shodan_host(ip):
     r = api.host(ip)
     request_time = time.time() - start
 
-    data = []
-    for i, elem in enumerate(r['data']):
-        data.append({})
-        data[i].update({'product': elem.get('product')})
-        data[i].update({'os': elem.get('os')})
-        data[i].update({'banner': elem.get('data')})
-        data[i].update({'timestamp': elem.get('timestamp')})
-        data[i].update({'port': elem.get('port')})
-        data[i].update({'org': elem.get('org')})
-
     result = {
-        'request_time': request_time,
-        'ip': r['ip_str'],
-        'country_name': r['country_name'],
-        'hostnames': r['hostnames'],
-        'data': data
+        'request_time': fix_slash(request_time),
+        'ip': fix_slash(r['ip_str']),
+        'country_name': fix_slash(r['country_name']),
+        'hostnames': fix_slash(r['hostnames'])
     }
 
+    for i, elem in enumerate(r['data']):
+        result.update({f'product_{i}': fix_slash(elem.get('product'))})
+        result.update({f'os_{i}': fix_slash(elem.get('os'))})
+        result.update({f'banner_{i}': fix_slash(elem.get('data'))})
+        result.update({f'timestamp_{i}': fix_slash(elem.get('timestamp'))})
+        result.update({f'port_{i}': fix_slash(elem.get('port'))})
+        result.update({f'org_{i}': fix_slash(elem.get('org'))})
+
     return result
+
+
+res = {'banner_0': 'HTTP/1.1 404 Not Found\\r\\nDate: Sun, 22 Nov 2020 21:12:34 '
+       'GMT\\r\\nContent-Type: text/html\\r\\nContent-Length: '
+       '536\\r\\nConnection: keep-alive\\r\\n\\r\\n',
+       'banner_1': 'HTTP/1.1 404 Not Found\\r\\nDate: Sat, 31 Oct 2020 23:06:40 '
+       'GMT\\r\\nContent-Type: text/html\\r\\nContent-Length: '
+       '536\\r\\nConnection: keep-alive\\r\\n\\r\\n',
+       'country_name': 'Russia',
+       'hostnames': '[]',
+       'ip': '62.76.6.38',
+       'org_0': 'State Educational institution for High professiona',
+       'org_1': 'State Educational institution for High professiona',
+       'os_0': 'None',
+       'os_1': 'None',
+       'port_0': '443',
+       'port_1': '80',
+       'product_0': 'None',
+       'product_1': 'None',
+       'request_time': '1.001835823059082',
+       'timestamp_0': '2020-11-22T21:12:34.884168',
+       'timestamp_1': '2020-10-31T23:06:41.029571'}
+
+# res = shodan_host('62.76.6.38')  # 142.93.33.2
+# pprint(res)
+
+
+csv_writer(res)
+print(csv_reader())
